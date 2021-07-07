71ce0df68f8d58cf4bb46a8613b150219ac1c2d5
Trace <tracedelange@me.com>
2021-07-07 09:18:50 -0500

    commit tree 00b3e355cdfb23d0285246cd4da03c96dd3dcdc6

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,15 @@
 import React from "react";
+import * as data from '../data/user';
+
 
 function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
       <p>I Made This</p>
-      <img src={image} alt="I made this" />
+      <img src={data.image} alt="I made this" />
     </div>
   );
 }
+
+export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
+import * as data from '../data/user';
 
 function Home() {
   return (
     <div id="home">
       <h1>
-        {username} is a Web Developer from {city}
+        {data.username} is a Web Developer from {data.city}
       </h1>
     </div>
   );
 }
+
+export default Home;
